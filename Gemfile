65eb1ea8e63d205500f4a93f950b64722e65cf5c
thaotonto <ots.hunter@gmail.com>
2017-11-19 10:46:02 +0700

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -11,6 +11,12 @@ gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 gem 'bcrypt', '3.1.11'
+
+gem 'faker'
+
+gem 'will_paginate'
+
+gem 'bootstrap-will_paginate'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114120032) do
+ActiveRecord::Schema.define(version: 20171119034404) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171114120032) do
     t.integer "gender"
     t.string "dob"
     t.string "remember_digest"
+    t.boolean "admin"
   end
 
 end
